96c8aa43444330246f7d8c204145b442d3415720
Mergen Nachin <mnachin@meta.com>
2026-03-06 14:36:14 -0500

    Address comments by malfet

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -15838,6 +15838,26 @@ def forward(self, arg0_1: "Sym(s77)", arg1_1: "Sym(s27)", arg2_1: "Sym(s53)", ar
             out2 = run_session(100, 16, 64, self.device)
             self.assertEqual(out2.device.type, self.device)
 
+    def test_half_reduction(self):
+        for dtype in [torch.float16, torch.bfloat16]:
+            if not self.is_dtype_supported(dtype):
+                continue
+            self.common(
+                lambda x: x.sum(),
+                (make_tensor(1024, dtype=dtype, device=self.device),),
+                check_lowp=False,
+            )
+
+    def test_half_where(self):
+        for dtype in [torch.float16, torch.bfloat16]:
+            if not self.is_dtype_supported(dtype):
+                continue
+            self.common(
+                lambda x: torch.where(x > 0.5, x, x.new_zeros(())),
+                (make_tensor(1024, dtype=dtype, device=self.device),),
+                check_lowp=False,
+            )
+
     # end of class CommonTemplate - add new tests here
 
 
